879e56bc07cbde4f935442d9d021c0a688750af8
Luke Sheppard <sheppiegit@gmail.com>
2023-07-18 15:03:12 -0400

    Added a upgradebox script which upgrades from boxv1 to boxv2.

--- script/UpgradeBox.s.sol
+++ script/UpgradeBox.s.sol
@@ -2,7 +2,25 @@
 pragma solidity 0.8.18;
 
 import "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {BoxV2} from "../src/BoxV2.sol";
+import {BoxV1} from "../src/BoxV1.sol";
 
 contract UpgradeBox is Script {
-    function run() external returns (address) {}
+    function run() external returns (address) {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("ERC19", block.chainid);
+        vm.startBroadcast();
+        BoxV2 box2 = new BoxV2();
+        vm.stopBroadcast();
+        address proxy = upgradeBox(mostRecentlyDeployed, address(box2));
+        return proxy;
+    }
+
+    function upgradeBox(address proxyAddress, address box2) public returns (address) {
+        vm.startBroadcast();
+        BoxV1 proxy = BoxV1(proxyAddress);
+        proxy.upgradeTo(address(box2));
+        vm.stopBroadcast();
+        return address(proxy);
+    }
 }
