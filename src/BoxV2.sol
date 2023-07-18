dd666485ebb0ed5adb4b9813ea94be54ef5e1f0c
Luke Sheppard <sheppiegit@gmail.com>
2023-07-18 14:40:58 -0400

    boxv2 setup.

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract BoxV2 {
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV2 is UUPSUpgradeable {
     uint256 internal number;
 
     function setNumber(uint256 _number) external {
@@ -15,4 +17,6 @@ contract BoxV2 {
     function version() external pure returns (uint256) {
         return 1;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
