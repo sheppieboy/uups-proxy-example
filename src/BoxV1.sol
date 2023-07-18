a29dfd1514ee22317bfb482926f18fd774455240
Luke Sheppard <sheppiegit@gmail.com>
2023-07-18 14:28:59 -0400

    Added the UUPSUpgradeable.

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract BoxV1 {
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract BoxV1 is UUPSUpgradeable {
     uint256 internal number;
 
     function getNumber() external view returns (uint256) {
@@ -11,4 +14,6 @@ contract BoxV1 {
     function version() external pure returns (uint256) {
         return 1;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
