0290d9bb5dce30a655226954dc08ee06a3043812
Luke Sheppard <sheppiegit@gmail.com>
2023-07-18 14:38:40 -0400

    Added uinitializable and owneabl;e.

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -3,9 +3,20 @@ pragma solidity 0.8.18;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 
-contract BoxV1 is UUPSUpgradeable {
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
+
+    //@custom:0z-upgrades-sunsale-allow-constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(); //sets owner as msg.sender
+        __UUPSUpgradeable_init();
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
