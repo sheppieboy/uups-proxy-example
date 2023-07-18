963758a0f9af738f2e4a97ebaff25e7d579caac3
Luke Sheppard <sheppiegit@gmail.com>
2023-07-18 14:51:05 -0400

    added a deploy script for the first box and the proxy.

---
+++ script/DeployBox.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import "forge-std/Script.sol";
+import "../src/BoxV1.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract DeployBox is Script {
+    function run() external returns (address) {
+        address proxy = deployBox();
+        return proxy;
+    }
+
+    function deployBox() public returns (address) {
+        BoxV1 box1 = new BoxV1(); //implementation/logic
+        ERC1967Proxy proxy = new ERC1967Proxy(address(box1), "");
+        return address(proxy);
+    }
+}
